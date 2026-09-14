d396966cedcd9119d20fcd3502bbec7cec3bc378
SAM KAI RUI <kairuisam1012@gmail.com>
2026-09-14 18:34:33 +0800

    location service done

--- location-service/src/main/java/com/rideshare/locationservice/config/RedisConfig.java
+++ location-service/src/main/java/com/rideshare/locationservice/config/RedisConfig.java
@@ -1,5 +1,25 @@
 package com.rideshare.locationservice.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
 public class RedisConfig {
-    
+    @Bean
+    public RedisTemplate<String,String> redisTemplate(
+            RedisConnectionFactory connectionFactory
+    ){
+        RedisTemplate<String,String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        // String serializers make data humanReliable in Redis cli
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
 }
--- location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
+++ location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
@@ -5,10 +5,12 @@ import com.rideshare.locationservice.dto.NearByDriverResponse;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.geo.Point;
+import org.springframework.data.geo.*;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -54,11 +56,45 @@ public class LocationService {
     public List<NearByDriverResponse> findByDrivers(double latitude, double longitude, double radiusInKm) {
         log.info("Finding drivers near lat: {} long: {} withing {}Km",
                 latitude, longitude, radiusInKm);
-        
+
+        Circle searchArea = new Circle(
+                new Point(longitude,latitude),new Distance(radiusInKm, Metrics.KILOMETERS)
+        );
+
+        GeoResults<RedisGeoCommands.GeoLocation<String>> results =
+                redisTemplate.opsForGeo().radius(DRIVERS_GEO_KEY,
+                        searchArea,
+                        RedisGeoCommands.
+                                GeoRadiusCommandArgs.
+                                newGeoRadiusArgs().
+                                includeCoordinates().
+                                includeDistance().
+                                sortAscending().
+                                limit(10));
+
+        List<NearByDriverResponse> nearbyDrivers = new ArrayList<>();
+        if (results!=null){
+            results.getContent().forEach(result -> {
+                RedisGeoCommands.GeoLocation<String> location = result.getContent();
+                nearbyDrivers.add(new NearByDriverResponse(
+                        location.getName(),
+                        location.getPoint().getY(),
+                        location.getPoint().getX(),
+                        result.getDistance().getValue()
+                ));
+            });
+        }
+        log.info("Found {} drivers nearby", nearbyDrivers.size());
+        return nearbyDrivers;
     }
 
-    public void removeDriver(String driverID) {
-
+    /**
+     * Remove driver when they go offline
+     * Maps to Redis ZREM command.
+     */
+    public void removeDriver(String driverId) {
+        log.info("Removing driver: {}", driverId);
+        redisTemplate.opsForGeo().remove(DRIVERS_GEO_KEY,driverId);
     }
 
 
