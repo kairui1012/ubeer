aa99b26db3c8dd410a3f4870513aa5d25bb13d8e
SAM KAI RUI <kairuisam1012@gmail.com>
2026-09-15 17:48:32 +0800

    ride - service fully complete

--- ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/rideshare/rideservice/config/KafkaConfig.java
@@ -1,4 +1,33 @@
 package com.rideshare.rideservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    // Topic where Ride Service published ride request
+    // Matching services subscribe to this topic
+
+    @Bean
+    public NewTopic rideRequestedTopic(){
+        return TopicBuilder
+                .name("ride.request")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    // Topic where Matching Service published math results
+    // Ride Service subscribers to this topic
+    public NewTopic rideMatchedTopic(){
+        return TopicBuilder.name("ride.matched")
+                .partitions(3)
+                .replicas(1)
+                .build();
+
+    }
 }
